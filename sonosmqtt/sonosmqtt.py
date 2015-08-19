f3624e2ec0031f1953552536686c09e03e6c372a
Andrew Watson <andy@watsons.ch>
2015-08-19 20:26:58 +0200

    Listen for song changes on local players and publish updates
    List of players is populated once at startup, and any song changes
    following the first boot are published to the player's mqtt topic,
    under now_playing.
    
    Still missing :
      - scan network for new/deactivated players
      - watch for play/pause to clear the channel when a song is not playing
      - react to play/pause/next commands on the mqtt topic

--- sonosmqtt/sonosmqtt.py
+++ sonosmqtt/sonosmqtt.py
@@ -14,6 +14,7 @@ class MQTT_Client(object):
         self.user = mqtt_settings.get('username', None)
         self.password = mqtt_settings.get('password', None)
         self.sub_topic = mqtt_settings.get('topic') + "/#"
+        self.pub_topic = mqtt_settings.get('topic')
 
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
@@ -24,8 +25,6 @@ class MQTT_Client(object):
 
         self.client.connect(self.server, self.port, 60)
 
-        self.client.loop_forever()
-
     def on_connect(self, client, userdata, flags, rc):
         print ("Connected to mqtt server")
 
@@ -34,13 +33,38 @@ class MQTT_Client(object):
     def on_message(self, client, userdata, msg):
         print (msg.topic+" | "+str(msg.payload))
 
+    def publish(self, topic, message):
+        topic = "/".join((self.pub_topic, topic))
+        self.client.publish(topic, payload=message, qos=0, retain=True)
+
+    def loop(self):
+        self.client.loop(timeout=0.5)
+
 print settings
 devices = soco.discover()
 
 if __name__ == '__main__':
-    for dev in devices:
-        ct = dev.get_current_track_info()
-        print("{} : {} - {} ({})".format(dev.player_name, ct.get('artist'), 
-            ct.get('title'), ct.get('duration')))
 
     MQ = MQTT_Client(settings.get('mqtt'))
+    sonos_devs = dict()
+    for dev in devices:
+        ct = dev.get_current_track_info()
+        sonos_devs[dev] = ct.get('uri', None)
+
+    print("initial dictionary: ", sonos_devs)
+
+
+    while True:
+        # get current playing tracks from sonos
+        for dev, last_track_uri in sonos_devs.items():
+            ct = dev.get_current_track_info()
+            if ct.get('uri', None) != last_track_uri:
+                #print("{} : {} - {} ({})".format(dev.player_name, ct.get('artist'), 
+                    #ct.get('title'), ct.get('duration')))
+                sonos_devs[dev] = ct.get('uri', None)
+                MQ.publish(dev.player_name.lower() + "/now_playing",
+                "{} - {} ({})".format(ct.get('artist'), ct.get('title'),
+                    ct.get('duration')))
+
+        # push to mqtt
+        MQ.loop()
